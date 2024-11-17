a3c865c609aa4220520ee0151fb6089e56ae7bdc
Azrof88 <hmazrof@gmail.com>
2024-11-17 21:09:02 +0600

    Quiz Application Simple implementation is done

--- Desktop/QuizApplication/src/main/java/com/example/constants/AdminEmailPassword.java
+++
@@ -1,6 +0,0 @@
-package com.example.constants;
-
-public class AdminEmailPassword {
-    public static String email="admin@gmail.com";
-    public static String password="123456";
-}
---
+++ Desktop/Quiz_application/src/constants/DatabaseConstants.java
@@ -0,0 +1,6 @@
+package constants;
+
+public class DatabaseConstants {
+    public static final String CONNECTION_URL = "jdbc:sqlite:E:/Development-portfolio/Desktop/Quiz_application/quiz.db";
+    public static final String DRIVER_CLASS = "org.sqlite.JDBC";
+}
